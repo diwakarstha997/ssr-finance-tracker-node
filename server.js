e032956a3af752fdf6799400f2fdc4d1f16491c4
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-02-11 21:38:00 +1100

    Add homepage route and static file access

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 import express from 'express';
 
+import path from 'path';
+
+const __dirname = path.resolve();
+
 // Initialize express app
 const app = express();
 
 // Define a port
 const PORT = process.env.PORT || 3000;
 
+// Serve public file
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home page Route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"src","html","index.html"));
+})
+
 // Start the server 
 app.listen(PORT, (error) => {
     error
         ? console.log(`Error: ${error.message}`)
-        : console.log(`Server running on PORT:${PORT}`);
+        : console.log(`Server running on PORT:${PORT} \n http://localhost:${PORT}`);
 })
